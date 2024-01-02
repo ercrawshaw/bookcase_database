7b119c41fee0efb4316d88d5366c17cd0adeb196
Elle Crawshaw <ellecrawshaw@elles-air.home>
2024-01-02 16:52:26 +0000

    more work

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ const server = http.createServer((request, response) => {
           response.statusCode = 200;
           response.setHeader("Content-Type", "application/json");
           response.end(JSON.stringify({ foundBook: book }));
+        }else{
+          response.statusCode = 404;
+          response.setHeader("Content-Type", "application/json");
+          response.end(JSON.stringify(`error: ${response.statusCode} , book not found`))
         }
       });
     });
@@ -46,6 +50,8 @@ const server = http.createServer((request, response) => {
     request.on("data", (packet) => {
       body += packet;
     });
+
+
 
     request.on("end", async () => {
       try {
@@ -71,7 +77,63 @@ const server = http.createServer((request, response) => {
       }
     });
   }
+
+  if (url.endsWith('/author') && method === 'GET') {
+    const bookId = +url.match(/\d/g);
+    //get author ID from book ID in books.json
+    let authorId = 0;
+    fs.readFile("./data/books.json", "utf-8").then((data) => {
+      const bookData = JSON.parse(data);
+      
+      bookData.forEach((book) => {
+        if (book.bookId === bookId) {
+          authorId = book.authorId
+        }else{
+          response.statusCode = 404;
+          response.setHeader("Content-Type", "application/json");
+          response.end(JSON.stringify(`error: ${response.statusCode} , author not found`))
+        };
+      
+      });
+      fs.readFile("./data/authors.json", "utf-8").then((data) => {
+        const authorData = JSON.parse(data);
+
+        authorData.forEach((author) => {
+          if (author.authorId === authorId) {
+            response.statusCode = 200;
+            response.setHeader("Content-Type", "application/json");
+            response.end(JSON.stringify({'author' : author}))
+          }
+        })
+      })
+    });
+  }
+
+  if (url.startsWith('/api/books?fiction') && method === 'GET') {
+
+    const books = fs.readFile('./data/books.json', 'utf-8').then((data) => {
+      return JSON.parse(data);
+    })
+    Promise.all([books]).then((results) => {
+        //console.log(results[0])
+        const trueBooks = results[0].filter((book) => book.isFiction === true);
+        const falseBooks = results[0].filter((book) => book.isFiction === false);
+        if (url.endsWith('true')) {
+          response.statusCode = 200;
+          response.setHeader("Content-Type", "application/json");
+          response.end(JSON.stringify({fiction : trueBooks}))
+        }else if (url.endsWith('false')){
+          response.statusCode = 200;
+          response.setHeader("Content-Type", "application/json");
+          response.end(JSON.stringify({non_fiction : falseBooks}))
+        }else{
+          response.end(JSON.stringify(`Error Code: 400, invalid query`));
+        }
+    })
+  }
+
 });
+
 
 server.listen(6969, (err) => {
   if (err) {
